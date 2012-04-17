8b1dbab9a71095626b7d39c1ac0589d5b14c5f3a
Chad Etzel <chad@jazzychad.net>
2012-04-17 01:50:11 -0700

    need config.js in repo for heroku...

---
+++ config.js
@@ -0,0 +1,15 @@
+exports.port = process.env.PORT || 3000;
+exports.mongodb_uri = process.env.MONGOLAB_URI;
+exports.sitename = "0x10co.de";
+exports.links = [
+	{
+		title:	"0x10c Forum",
+		url:	"http://www.0x10cforum.com/"
+	},
+	{
+		title:	"0x10c Wiki",
+		url:	"http://0x10cwiki.com/"
+	}
+];
+// best to have an image of 38px height
+exports.logo = "img/logo.png";
